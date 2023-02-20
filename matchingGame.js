586e76555330da3059dd9159249cee77342aa68a
Emily <kimemily1227@gmail.com>
2023-02-20 13:17:49 -0800

    added js file for creating play cards on the board

---
+++ matchingGame.js
@@ -0,0 +1,9 @@
+for(var i = 0; i<4; i++){
+    for(var j = 0; j<2; j++){
+        var btn = document.createElement("Button");  
+        btn.setAttribute("class", "btn btn-success btnCard")
+        var board = document.getElementById("gameBoard").appendChild(btn);
+        //assign id here
+    }
+
+}
