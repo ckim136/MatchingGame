11a587ccd325b9d5be6694fb38361520b1065fdb
Emily <kimemily1227@gmail.com>
2023-02-20 23:22:38 -0800

    added flipcards method

--- matchingGame.js
+++ matchingGame.js
@@ -1,4 +1,5 @@
 var rndArray = [];
+var queue = [];
 
 for(var i = 0; i<4; i++){
     for(var j = 0; j<2; j++){
@@ -13,13 +14,19 @@ for(var i = 0; i<4; i++){
 
 function setId(btn){
     var assign = false;
+
     while(assign == false){
         var rnd = Math.floor(Math.random() * 4);
         if (!rndArray.includes(rnd) || rndArray.filter(x => x === rnd).length == 1 ){
             btn.id="card"+rnd;
             rndArray.push(rnd);
-            //create image
+
+            //configure image
             setPictures(btn, rnd);
+
+            //set onclick function
+            btn.onclick = function(){flipCards(btn)};
+
             assign = true;
         }else{
             assign = false;
@@ -32,4 +39,23 @@ function setPictures(btn, rnd){
     image.setAttribute("src", "img/animal" + rnd+ ".jpg");
     image.setAttribute("class", "imageCard")
     btn.appendChild(image);
+}
+
+function flipCards(btn){
+    console.log("flipped!!");
+    if(queue.length == 0){
+        queue.push(btn);
+    }else if(queue.length == 1){
+        queue.push(btn);
+        //check id
+        var first = queue[0].id.slice(-1);
+        var second = queue[1].id.slice(-1);
+        if(first == second){
+            console.log("correct!")
+        }
+        else{
+            console.log("incorrect!")
+        }
+        queue = [];
+    }
 }
